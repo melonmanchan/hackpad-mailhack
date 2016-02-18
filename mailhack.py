cbee73b5ad0b9f5d5ce203881dee1b1ddce1eb30
Matti Jokitulppo <melonmanchan@gmail.com>
2016-02-18 15:25:51 +0200

    Initial commit

---
+++ mailhack.py
@@ -0,0 +1,50 @@
+#!usr/bin/env python
+# Hack for sending Hackpad invitatiol emails
+
+import StringIO
+import csv
+import json
+import sendgrid
+import subprocess
+import time
+
+with open('config.json') as data_file:
+    config = json.load(data_file)
+
+sendgridclient = sendgrid.SendGridClient(config['sendgrid_key'])
+
+def send_mail(toMail, token):
+    verificationlink = config['hackpad_url'] + '/ep/account/validate-email?email=' + toMail + '&token=' + token
+
+    message = sendgrid.Mail(to=toMail, subject='Verifying your Aalto Hackpad account',
+                            html='Your verification link is <a href="{}">{}</a> . Happy hacking!'.format(verificationlink, verificationlink),
+                            from_email='noreply@layersbox.aalto.fi')
+    status, resp = sendgridclient.send(message)
+    print status
+    print resp
+
+def main():
+    while True:
+        mailfile = open('already_sent.txt', 'ab+')
+        filecontents = mailfile.read().splitlines()
+        cmdoutput = subprocess.check_output("docker exec -it " + config['container_name'] + " mysql  --batch  hackpad -e 'select email, token from email_signup;'", shell=True)
+        f = StringIO.StringIO(cmdoutput)
+        outputlist = [{k: v for k, v in row.items()}
+            for row in csv.DictReader(f, delimiter='\t')]
+
+        for i, val in enumerate(outputlist):
+            email = val['email']
+            token = val['token']
+
+            if not email in filecontents:
+                print 'inviting person ' + email
+                send_mail(email, token)
+
+                print 'appending... ' + email
+                mailfile.write(email + '\n')
+
+        mailfile.close()
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
